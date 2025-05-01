82d4755919f7019306e32625f5dc2fd78fe529b2
Daniel Coates <dcoates@scottlogic.com>
2025-05-01 16:47:01 +0100

    removed wildcard imports

--- friendface-web/src/test/java/com/example/friendface/UserControllerTest.java
+++ friendface-web/src/test/java/com/example/friendface/UserControllerTest.java
@@ -13,7 +13,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
