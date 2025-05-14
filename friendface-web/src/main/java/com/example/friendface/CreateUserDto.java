f5e851a4399d4d63a148ddb8eec95313de96ebae
Daniel Coates <dcoates@scottlogic.com>
2025-05-14 14:10:09 +0100

    removed unused imports

--- friendface-web/src/main/java/com/example/friendface/CreateUserDto.java
+++ friendface-web/src/main/java/com/example/friendface/CreateUserDto.java
@@ -2,11 +2,11 @@ package com.example.friendface;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
+
 import lombok.Getter;
 import lombok.Setter;
 
-import java.sql.Blob;
+
 
 
 
--- friendface-web/src/test/java/com/example/friendface/UserServiceTest.java
+++ friendface-web/src/test/java/com/example/friendface/UserServiceTest.java
@@ -1,14 +1,13 @@
 package com.example.friendface;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import org.mockito.MockitoAnnotations;
+
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
