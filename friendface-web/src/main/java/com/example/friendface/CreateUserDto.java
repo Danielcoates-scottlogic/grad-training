aa30baeb812a65921a8023f9c08a050f19171896
Daniel Coates <dcoates@scottlogic.com>
2025-05-14 14:09:32 +0100

    tests for user service

--- friendface-web/src/main/java/com/example/friendface/CreateUserDto.java
+++ friendface-web/src/main/java/com/example/friendface/CreateUserDto.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 import java.sql.Blob;
 
 
-@Builder
+
 @Setter
 @Getter
 public class CreateUserDto {
@@ -17,7 +17,6 @@ public class CreateUserDto {
     @NotBlank(message = "Username Required")
     @Size(min = 3, max = 99)
     private String username;
-    @Builder.Default
     private String colour = "#000000";
     @NotBlank(message = "Password required")
     @Size(min = 3, max = 99)
